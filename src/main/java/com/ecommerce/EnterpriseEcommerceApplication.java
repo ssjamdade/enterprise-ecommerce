bea966bbb17e0278dc3695bd8e557b3404c7bf0a
ssjamdade <jamdadesanket110@gmail.com>
2026-07-08 21:07:02 +0530

    setup project and configured database connection

---
+++ src/main/java/com/ecommerce/EnterpriseEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseEcommerceApplication.class, args);
+	}
+
+}
